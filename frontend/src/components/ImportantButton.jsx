c2d1fdae64792e80c62e3d3335eb93655cac6bc5
sifeddineEddr <sifeddine.edr@gmail.com>
2024-02-07 16:02:03 +0100

    main button animation finished

--- frontend/src/components/ImportantButton.jsx
+++ frontend/src/components/ImportantButton.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function ImportantButton({ label, action }) {
   return (
-    <button
-      className="px-20 py-2 rounded-3xl bg-brandRed shadow-3xl border-2 border-brandRed font-semibold text-lg"
+    <motion.button
+      whileHover={{ opacity: 0.9, scale: 1.05 }}
+      whileTap={{ y: 5 }}
+      className="px-20 py-2 rounded-3xl bg-brandRed shadow-3xl border-2 border-brandRed font-semibold text-lg capitalize"
       onClick={action}
     >
       {label}
-    </button>
+    </motion.button>
   );
 }
