eaa8824cb43ec39d688f49505ae12ee4e396086f
sifeddineEddr <sifeddine.edr@gmail.com>
2024-02-14 12:24:36 +0100

    user controller set up

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+    }
+
+    public function oauthRegister(Request $request)
+    {
+    }
+
+    public function login(Request $request)
+    {
+    }
+
+    public function oauthLogin(Request $request)
+    {
+    }
+
+    public function logout(Request $request)
+    {
+    }
+
+    public function oauthLogout(Request $request)
+    {
+    }
+}
